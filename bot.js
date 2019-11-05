733f85a204d35c5e29070dafd26aa9cc7b863c54
AitoW7372 <57394405+AitoW7372@users.noreply.github.com>
2019-11-05 14:16:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.TgDFULirvv0w2b97ihNGoxqZkXsy026c);//BOT_TOKEN is the Client Secret
